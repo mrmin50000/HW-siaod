c7d3b6de76cbf29904ddcd0ae93443d0f62f9320
mrmin50000 <danilast2007@gmail.com>
2025-09-14 20:28:16 +0300

    map

--- task_5_2/ex_1.cpp
+++ task_5_2/ex_1.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <random>
@@ -24,12 +25,17 @@ std::string genRandString(int len) {
 	return str;
 }
 
-std::string getFioAndAddress(std::unordered_map<int, std::string> map, int billNumber) {
+std::string getFioAndAddress(std::unordered_map<int, std::pair<int, std::string>> map, int billNumber) {
 
 	int left = 0, right = map.size(), mid;
 	while (left <= right) {
 		mid = (left + right) / 2;
-		if (map[])
+		if (map[mid].first == billNumber)
+			return map[mid].second;
+		else if (map[mid].first > billNumber)
+			right = mid - 1;
+		else
+			left = mid + 1;
 	}	
 	return "not found";
 }
@@ -38,10 +44,11 @@ int main() {
 	std::string result;
 	std::ofstream inputFile("file.bin", std::ios::binary);
 	int billNumberInput, n;
-	std::unordered_map<int, std::string> map;
+	std::unordered_map<int, std::pair<int, std::string>> map;
+	std::vector<std::pair<int, std::string>> forMap;
 	std::cin >> n;
 	for (int i = 0; i < n; ++i) {
-		if (i == n / 2)
+		if (i == n / 8)
 			billNumberInput = htobe32(8888888);
 		else
 			billNumberInput = htobe32(genRandNumber(1000000, 9999999));
@@ -60,9 +67,17 @@ int main() {
 	while (outputFile.read((char *)&billNumber, sizeof(int))) {
 		int i = be32toh(billNumber);
 		std::getline(outputFile, data);
-		map[i] = data;
+		forMap.push_back(std::pair(i, data));
 	}
 	outputFile.close();
+	
+	std::sort(forMap.begin(), forMap.end());
+	for (int i = 0;i < forMap.size(); ++i) {
+		map[i] = forMap[i];
+	}
+
+	std::cout << getFioAndAddress(map, 8888888) << '\n';
+
 	auto end = std::chrono::system_clock::now();
 	std::chrono::duration<double> total = end - start;
 	std::cout << "Total time: " << total.count() << "sec\n";
